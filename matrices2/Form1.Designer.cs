691956dd9eda18f6c6e37ffcd5e064b08b470279
Evgeny Samoilov <zs.dp.ua@gmail.com>
2021-10-16 08:36:04 +0300

    добавил элемкнты упровления added controls

--- matrices2/Form1.Designer.cs
+++ matrices2/Form1.Designer.cs
@@ -29,13 +29,148 @@ namespace matrices2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RemoveMatrix = new System.Windows.Forms.Button();
+            this.Determinant = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.CreateMatrix = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(602, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "label3";
+            // 
+            // RemoveMatrix
+            // 
+            this.RemoveMatrix.Location = new System.Drawing.Point(665, 134);
+            this.RemoveMatrix.Name = "RemoveMatrix";
+            this.RemoveMatrix.Size = new System.Drawing.Size(60, 40);
+            this.RemoveMatrix.TabIndex = 16;
+            this.RemoveMatrix.Text = "Remove matrix";
+            this.RemoveMatrix.UseVisualStyleBackColor = true;
+            // 
+            // Determinant
+            // 
+            this.Determinant.Location = new System.Drawing.Point(605, 180);
+            this.Determinant.Name = "Determinant";
+            this.Determinant.Size = new System.Drawing.Size(120, 40);
+            this.Determinant.TabIndex = 15;
+            this.Determinant.Text = "Determinant of the matrix";
+            this.Determinant.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(601, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "columns";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "rows";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(658, 283);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(659, 102);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // CreateMatrix
+            // 
+            this.CreateMatrix.Location = new System.Drawing.Point(604, 134);
+            this.CreateMatrix.Name = "CreateMatrix";
+            this.CreateMatrix.Size = new System.Drawing.Size(60, 40);
+            this.CreateMatrix.TabIndex = 10;
+            this.CreateMatrix.Text = "Create matrix";
+            this.CreateMatrix.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RemoveMatrix);
+            this.Controls.Add(this.Determinant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.CreateMatrix);
+            this.Name = "Form1";
+            this.Text = "matrices";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button RemoveMatrix;
+        private System.Windows.Forms.Button Determinant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button CreateMatrix;
     }
 }
 
